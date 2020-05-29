7718a5516d8c79766ce3fe38660de11d8c29a525
lesti <yaseloklass@gmail.com>
2020-05-29 13:55:27 +0300

    Исправлено.

--- lab№6С++/lab№6С++.cpp
+++ lab№6С++/lab№6С++.cpp
@@ -3,9 +3,13 @@
 #include <string>
 #include <windows.h>
 
+#include <algorithm>
+
+
+
 using namespace std;
 
-FILE* fout, *fin;
+FILE* fout, *fin; //объекты для записи данных в файл и чтение их из файла
 
 void Task1()
 {
@@ -25,9 +29,9 @@ void Task1()
 	Workers *workers = new Workers[number];
 	for (int i = 0; i < number; i++)
 	{
-		cin.ignore();
+		cin.ignore(); // для ввода нескольких слов в одной строке
 		cout << "Фамилия: ";
-		getline(cin, workers[i].surname);
+		getline(cin, workers[i].surname); //ввод данных в элемент структуры
 		do
 		{
 			cout << "Возраст: ";
@@ -47,10 +51,10 @@ void Task1()
 		getline(cin, workers[i].gender);
 	}
 
-	fout = fopen("Workers.bin", "wb");
-	fwrite(workers, sizeof(Workers), number, fout);
+	fout = fopen("Workers.bin", "wb"); //открываем файл
+	fwrite(workers, sizeof(Workers), number, fout); //запись в файл
 	fclose(fout);
-	delete[] workers;
+	delete[] workers; //освобождаем память
 	workers = nullptr;
 	Workers* restored = new Workers[number];
 	fin = fopen("Workers.bin", "rb");
@@ -59,7 +63,8 @@ void Task1()
 	cout << endl << "Информация о всех работниках женского пола без высшего образования, которым в этом году необходимо оформлять пенсию:" << endl << endl;
 	for (int i = 0; i < number; i++)
 	{
-		if (restored[i].age >= 60 && restored[i].gender == "женский" && restored[i].education != "высшее")
+		std :: transform(restored[i].education.begin(), restored[i].education.end(), restored[i].education.begin(), tolower);
+		if (restored[i].age >= 60 && restored[i].gender._Equal( "женский") && !restored[i].education._Equal("высшее"))
 		{
 			cout << "Работник №" << i + 1 << endl;
 			cout << "Фамилия: " << restored[i].surname << " " << endl;
@@ -74,46 +79,47 @@ void Task1()
 ///////////////////////////////////////
 void Task2()
 {
-	int matrix[5][5];
+	const int n = 5, m = 5;
+	int matrix[n][m];
 	cout << "Введите элементы матрицы:" << endl;
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < 5; j++)
+		for (int j = 0; j < m; j++)
 		{
 			/*cin >> matrix[i][j];*/
-			matrix[i][j] = rand() % 10;
+			matrix[i][j] = rand() % 100;
 		}
 	}
 	fout = fopen("matrix.bin", "wb");
-	fwrite(matrix, sizeof(int), 25, fout);
+	fwrite(matrix, sizeof(int), n*m, fout);
 	fclose(fout);
-	int restored[5][5];
+	int restored[n][m];
 	fin = fopen("matrix.bin", "rb");
-	fread(restored, sizeof(int), 25, fin);
+	fread(restored, sizeof(int), n*m, fin);
 	fclose(fin);
 	cout << endl << "Матрица которую вы ввели: " << endl;
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < 5; j++)
+		for (int j = 0; j < m; j++)
 		{
 			cout << restored[i][j] << "\t";
 		}
 		cout << endl << endl;
 	}
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < 5; j++)
+		for (int j = 0; j < m; j++)
 		{
 			restored[i][j] *= 2;
 		}
 	}
 	fout = fopen("matrix.bin", "wb");
-	fwrite(restored, sizeof(int), 25, fout);
+	fwrite(restored, sizeof(int), n*m, fout);
 	fclose(fout);
 	cout << endl << endl << "Матрица, елементы которой увеличены в 2 раза:" << endl;
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < 5; j++)
+		for (int j = 0; j < m; j++)
 		{
 			cout << restored[i][j] << "\t";
 		}
@@ -172,7 +178,7 @@ void Task3()
 	{
 		if (InternetNetwork[i].number_of_months_forward >= 3)
 		{
-			InternetNetwork[i].cost_per_month = InternetNetwork[i].cost_per_month - InternetNetwork[i].cost_per_month*0.07;
+			InternetNetwork[i].cost_per_month = (InternetNetwork[i].cost_per_month - InternetNetwork[i].cost_per_month*0.07);
 		}
 	}
 	fout = fopen("Internet.bin", "wb");
@@ -181,7 +187,7 @@ void Task3()
 	fin = fopen("Internet.bin", "rb");
 	fread(InternetNetwork, sizeof(Internet_Network), number, fin);
 	fclose(fin);
-	cout << endl << "Инфопмация о клиентах со снижением тарифа на 7%, если он оплатил на 3 месяца вперед:" << endl << endl;
+	cout << endl << "Информация о клиентах со снижением тарифа на 7%, если он оплатил на 3 месяца вперед:" << endl << endl;
 	for (int i = 0; i < number; i++)
 	{
 		cout << "Клиент №" << i + 1 << ": " << endl;
@@ -191,7 +197,7 @@ void Task3()
 		cout << "Количество месяцев, оплаченых наперед: " << InternetNetwork[i].number_of_months_forward << endl;
 		cout << "-------------------------------------" << endl;
 	}
-}
+}   
 
 int main()
 {
